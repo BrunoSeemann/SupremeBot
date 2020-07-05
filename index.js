a3ff9f5673c34508b8436b823f3fe28d104e06ea
Bruno Seemann <65774228+BrunoSeemann@users.noreply.github.com>
2020-07-05 16:56:01 -0300

    Add files via upload
    Libs used till now: Discord.js, superagent, fs

--- index.js
+++ index.js
@@ -1,14 +1,37 @@
 const botconfig = require("./botconfig.json");
 const Discord = require("discord.js");
 const colours = require("./colours.json");
+const superagent = require("superagent")
 
 const bot = new Discord.Client({disableEveryone: true});
 
 
 bot.on("ready", async () => {
     console.log(`${bot.user.username} is online`)
-    bot.user.setActivity("People die in the furnace", {url:"https://www.twitch.tv/yoda", type:"WATCHING"});
+    bot.user.setActivity("%help", {url:"https://www.twitch.tv/yoda", type:"WATCHING"});
 })
+
+const fs = require("fs");
+bot.commands = new Discord.Collection();
+bot.aliases = new Discord.Collection();
+fs.readdir("./commands", (err, files) => {
+
+    if (err) console.log(err)
+
+    let jsfile = files.filter(f => f.split(".").pop() === "js")
+    if(jsfile.length <= 0) {
+        return console.log("[LOGS] Couldn't Find Commands!");
+    }
+
+    jsfile.forEach((f, i) => {
+        let pull = require(`./commands/${f}`)
+        bot.commands.set(pull.config.name, pull);
+        pull.config.aliases.forEach(alias => {
+            bot.aliases.set(alias, pull.config.name)
+        });
+    });
+
+});
 
 bot.on("message", async message => {
     if (message.author.bot || message.channel.type === "dm") return;
@@ -18,42 +41,10 @@ bot.on("message", async message => {
     let cmd = messageArray[0];
     let args = messageArray.slice(1);
 
-    if (cmd === `${prefix}hello`) {
-        return message.channel.send("Hello")
-    }
+    if(!message.content.startsWith(prefix)) return;
+    let commandfile = bot.commands.get(cmd.slice(prefix.length)) || bot.commands.get(bot.aliases.get(cmd.slice(prefix.length)))
+    if(commandfile) commandfile.run(bot,message,args)
 
-    if (cmd === `${prefix}serverinfo`) {
-        let sEmbed = new Discord.RichEmbed()
-            .setColor(colours.cyan)
-            .setTitle("Server Info")
-            .setThumbnail(message.guild.iconURL)
-            .setAuthor(`${message.guild.name} Info`, message.guild.iconURL)
-            .addField("**Guild Name:**", `${message.guild.name}`)
-            .addField("**Guild Owner:**", `${message.guild.owner}`)
-            .addField("**Member Count:**", `\n${message.guild.memberCount}`, true)
-            .addField("**Role Count:**", `${message.guild.roles.size}`, true)
-            .addField("**Region**", `${message.guild.region}`)
-            .setFooter(`SupremeBot | Supremacy`, bot.user.displayAvatarURL, );
-        message.channel.send({embed: sEmbed});
-    }
-
-    if (cmd === `${prefix}userinfo`) {
-        let uEmbed = new Discord.RichEmbed()
-            .setColor(colours.red_light)
-            .setTitle("User Info")
-            .setThumbnail(message.author.displayAvatarURL)
-            .setAuthor(`${message.author.username} Info`, message.author.displayAvatarURL)
-            .addField("**Username:**", `${message.author.username}`, true)
-            .addField("**Discriminator:**", `#${message.author.discriminator}`, true)
-            .addField("**ID:**", `${message.author.id}`)
-            .addField("**Status:**", `${message.author.presence.status}`, true)
-            .addField("**In game**", `${message.author.presence.game}`, true)
-            .addField("**Created At:**", `${message.author.createdAt}`)
-            .setFooter(`SupremeBot | Supremacy`, bot.user.displayAvatarURL, );
-        message.channel.send({embed: uEmbed});
-
-    }
-
-})
+})  
 
 bot.login(botconfig.token);
